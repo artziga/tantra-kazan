769dd0d6cfc65991b0109fff17ef23fcaea84883
zai <artziga77@gmail.com>
2023-12-02 14:04:28 +0300

    корректировка структуры проекта. добавлена папка app

--- tantrakazan/feedback/widgets.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class StarRatingWidget(forms.Widget):
-    template_name = 'feedback/widgets/star_rating.html'
--- tantrakazan/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from main.views import index, ContactUsView
-
-app_name = 'main'
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('contact_us/', ContactUsView.as_view(), name='contact-us')
-]
