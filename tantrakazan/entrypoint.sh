5212f42e05540fd774eee9623722870277168e69
zai <artziga77@gmail.com>
2023-12-14 20:33:08 +0300

    исправлен баг фильтрации

--- tantrakazan/entrypoint.sh
+++ tantrakazan/entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 poetry run python tantrakazan/manage.py makemigrations --noinput
 poetry run python tantrakazan/manage.py migrate --noinput
 #poetry run python manage.py collectstatic --noinput
