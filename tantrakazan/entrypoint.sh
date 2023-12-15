87d5181eb770f87789eeb18445ffb2a97f3715b1
zai <artziga77@gmail.com>
2023-12-15 12:17:46 +0300

    игнор

--- tantrakazan/entrypoint.sh
+++ tantrakazan/entrypoint.sh
@@ -10,9 +10,8 @@ then
 
     echo "PostgreSQL started"
 fi
-
-poetry run python tantrakazan/manage.py makemigrations --noinput
-poetry run python tantrakazan/manage.py migrate --noinput
+#poetry run python tantrakazan/manage.py makemigrations --noinput
+#poetry run python tantrakazan/manage.py migrate --noinput
 #poetry run python manage.py collectstatic --noinput
 #poetry run gunicorn backpack_store.wsgi:application --bind 0.0.0.0:8000
 poetry run python tantrakazan/manage.py runserver 0.0.0.0:8000
